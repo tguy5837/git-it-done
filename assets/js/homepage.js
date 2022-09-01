e073c4498fc396d2d1e936ce1cb816373da201b0
DTGuy <tguy5837@gmail.com>
2022-08-31 21:14:11 -0400

    confirm getFeaturedRepos function works

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -28,6 +28,20 @@ var getUserRepos = function (user) {
         })
 };
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found.');
+        };
+    });
+}
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
